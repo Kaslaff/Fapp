9ffdf6a7489bb6a32970275a4c96b325134dc7b9
Oswald <oswald.capriles46@gmail.com>
2017-01-25 05:01:47 -0400

    Fixed rollback problem with foreigns keys

--- api/database/migrations/2017_01_25_200317_add_account_test_grant.php
+++ api/database/migrations/2017_01_25_200317_add_account_test_grant.php
@@ -14,8 +14,8 @@ class AddAccountTestGrant extends Migration
     public function up()
     {
         Schema::table('sites', function (Blueprint $table) {
-            $table->foreign('last_tested')->references('id')->on('accounts');
-            $table->foreign('last_granted')->references('id')->on('accounts');
+            $table->foreign('last_tested')->references('id')->on('accounts')->onDelete('cascade');
+            $table->foreign('last_granted')->references('id')->on('accounts')->onDelete('cascade');
         });
     }
 
@@ -27,7 +27,8 @@ class AddAccountTestGrant extends Migration
     public function down()
     {
         Schema::table('sites', function (Blueprint $table) {
-            //
+            $table->dropForeign(['last_tested']);
+            $table->dropForeign(['last_granted']);
         });
     }
 }
